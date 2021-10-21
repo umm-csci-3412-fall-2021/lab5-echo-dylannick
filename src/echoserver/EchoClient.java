d7603067d4b165a72c8118845873d034ba9af13b
Dylan531 <crame160@morris.umn.edu>
2021-10-21 16:20:53 -0500

    Tried to implement a method for the server to gracefully shut down

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,23 +1,29 @@
 package echoserver;
 import java.net.*;
 import java.io.*;
+import java.util.Objects;
+import java.util.Scanner;
 
 class EchoClient{
     public static void main(String[] args){
         try {
-
-
+            int b = 0;
+            InputStream consoleInput = System.in;
             Socket socket = new Socket("localhost", 3501);//connect to ServerSocket
 
-            InputStream consoleInput = System.in;
 
-            InputStream fromServer = socket.getInputStream();
-            OutputStream toServer = socket.getOutputStream();  //generate InputStream from Socket
 
-            toServer.write(consoleInput.read());  // Write what we input into the console directly to the server
-            System.out.println(fromServer.read()); // Print the response from the server
+            while(b != 255) {
+                InputStream fromServer = socket.getInputStream();
+                OutputStream toServer = socket.getOutputStream();  //generate InputStream from Socket
 
-            socket.close();
+                b = consoleInput.read();
+
+                toServer.write(b);  // Write what we input into the console directly to the server
+                System.out.println(fromServer.read()); // Print the response from the server
+            }
+
+            socket.shutdownOutput();
 
         } catch(Exception e){
             System.out.println(e);
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,8 +7,8 @@ class EchoServer{
         try {
 
             ServerSocket ss = new ServerSocket(3501);//declare a new ServerSocket on port 3500
-            Socket socket = ss.accept( );  //open the ServerSocket to receive connections
-            OutputStream toClient = socket.getOutputStream( );
+            Socket socket = ss.accept();  //open the ServerSocket to receive connections
+            OutputStream toClient = socket.getOutputStream();
             InputStream fromClient = socket.getInputStream();  //generate an outputstream from the Socket
 
             int b = 0;
@@ -17,7 +17,8 @@ class EchoServer{
                 System.out.println("byte: " + b);
                 toClient.write(b);  //write the array
             }
-            toClient.close();
+
+            toClient.flush();
 
         }catch(Exception e){
              System.out.println(e);
