0a55f7f6ff0ed8386bc51938d9e0971b8ae14dd6
Dylan531 <crame160@morris.umn.edu>
2021-10-21 12:19:15 -0500

    Made the client print what was echo'd from the server. Cleaned up the code to make the variable names a lot more clear.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -4,19 +4,22 @@ import java.io.*;
 
 class EchoClient{
     public static void main(String[] args){
-        try{
+        try {
 
-            Socket s = new Socket("localhost", 3501);//connect to ServerSocket
-            InputStream is = System.in;
-            OutputStream os = s.getOutputStream();  //generate InputStream from Socket
-            byte[] b = new byte[1];
-            is.read(b);
-            os.write(b);  //read byte array into array b
-            System.out.println("byte 1: " + b[0]); //print what was received
-            is.close();  //close everything
-            s.close();
 
-        }catch(Exception e){
+            Socket socket = new Socket("localhost", 3501);//connect to ServerSocket
+
+            InputStream consoleInput = System.in;
+
+            InputStream fromServer = socket.getInputStream();
+            OutputStream toServer = socket.getOutputStream();  //generate InputStream from Socket
+
+            toServer.write(consoleInput.read());  // Write what we input into the console directly to the server
+            System.out.println(fromServer.read()); // Print the response from the server
+
+            socket.close();
+
+        } catch(Exception e){
             System.out.println(e);
          }
     }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -4,24 +4,21 @@ import java.io.*;
 
 class EchoServer{
     public static void main(String[] args){
-        try{
+        try {
 
             ServerSocket ss = new ServerSocket(3501);//declare a new ServerSocket on port 3500
-            Socket s = ss.accept( );  //open the ServerSocket to receive connections
-            OutputStream os = s.getOutputStream( );
-            InputStream is = s.getInputStream();  //generate an outputstream from the Socket
-            // byte[] b = new byte[2];
-            // b[0] = 1;
-            // b[1] = 2;
+            Socket socket = ss.accept( );  //open the ServerSocket to receive connections
+            OutputStream toClient = socket.getOutputStream( );
+            InputStream fromClient = socket.getInputStream();  //generate an outputstream from the Socket
+
             int b = 0;
             while(b != -1){
-                b = is.read();
-                System.out.println("byte 1: " + b);
-                os.write(b);  //write the array
-                os.close();   //close everything
-    
+                b = fromClient.read();
+                System.out.println("byte: " + b);
+                toClient.write(b);  //write the array
             }
-            
+            toClient.close();
+
         }catch(Exception e){
              System.out.println(e);
          }
