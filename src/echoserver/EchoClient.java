d765e8a9b5d20e595f1b2f75498f487377a55ada
gilb0578 <ninjamipsmurf@gmail.com>
2021-10-21 11:29:24 -0500

    Wrote first implementation, takes the first byte and send it to the server

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,23 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
-public class EchoClient {
+class EchoClient{
+    public static void main(String[] args){
+        try{
+
+            Socket s = new Socket("localhost", 3501);//connect to ServerSocket
+            InputStream is = System.in;
+            OutputStream os = s.getOutputStream();  //generate InputStream from Socket
+            byte[] b = new byte[1];
+            is.read(b);
+            os.write(b);  //read byte array into array b
+            System.out.println("byte 1: " + b[0]); //print what was received
+            is.close();  //close everything
+            s.close();
+
+        }catch(Exception e){
+            System.out.println(e);
+         }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,30 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
-public class EchoServer {
+class EchoServer{
+    public static void main(String[] args){
+        try{
+
+            ServerSocket ss = new ServerSocket(3501);//declare a new ServerSocket on port 3500
+            Socket s = ss.accept( );  //open the ServerSocket to receive connections
+            OutputStream os = s.getOutputStream( );
+            InputStream is = s.getInputStream();  //generate an outputstream from the Socket
+            // byte[] b = new byte[2];
+            // b[0] = 1;
+            // b[1] = 2;
+            int b = 0;
+            while(b != -1){
+                b = is.read();
+                System.out.println("byte 1: " + b);
+                os.write(b);  //write the array
+                os.close();   //close everything
+    
+            }
+            
+        }catch(Exception e){
+             System.out.println(e);
+         }
+   }
+ 
 }
